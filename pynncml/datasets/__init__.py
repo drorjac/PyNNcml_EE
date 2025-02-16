0528e55dc816c38f6380b433425f7f77491bda5a
Amitgon <amitgonda@mail.tau.ac.il>
2025-02-16 15:33:20 +0200

    Update __init__.py

--- pynncml/datasets/__init__.py
+++ pynncml/datasets/__init__.py
@@ -1,6 +1,6 @@
 from pynncml.datasets.meta_data import MetaData
 from pynncml.datasets.link_data import Link, LinkMinMax, read_open_cml_dataset
 from pynncml.datasets.sensors_set import LinkSet, PointSet
-from pynncml.datasets.loaders import load_open_mrg, loader_open_mrg_dataset
+from pynncml.datasets.loaders import load_open_mrg, loader_open_mrg_dataset, load_open_mrg1, loader_open_mrg_dataset1
 from pynncml.datasets.link_processing import linkdataset2subsequent
 # this is the file we want to see!
